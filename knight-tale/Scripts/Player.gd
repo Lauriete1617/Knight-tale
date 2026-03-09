09885643dd26fbdf450a2e1ee12ce8b5856bda41
Marilu <Maria1luiza1s.s@gmail.com>
2026-03-09 19:28:25 -0300

    Tentando fazer script player

--- knight-tale/Scripts/Player.gd
+++ knight-tale/Scripts/Player.gd
@@ -1,42 +1,25 @@
 extends CharacterBody2D
-@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
-@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
 
 
-const gravidade = 300
-const velocidade_pulo = -300
-const velocidade = 300.0
-var direção =0.0
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-# Imput de pulo
-func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("pulo") and is_on_floor():
-		velocity.y = velocidade_pulo
 
 func _physics_process(delta: float) -> void:
-	#Sistema de receber input do teclado = input andar pros lados
-	var input_teclado = Input.get_axis("esquerda", "direita")
-	
-	#sistema de anda
-	if input_teclado != 0: 
-		direção = input_teclado
-	#//
-
-	#processo velocidade*gravidade
-	if direção:
-		velocity.x = direção * velocidade
-		move_and_slide()
-	
+	# Add the gravity.
 	if not is_on_floor():
-		velocity.y += delta*gravidade
-	
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
 	move_and_slide()
