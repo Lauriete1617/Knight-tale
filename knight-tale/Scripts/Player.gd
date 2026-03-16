06b7c0bff82e8a01add9cdc6f0d04c88e567c004
Marilu <Maria1luiza1s.s@gmail.com>
2026-03-16 20:25:50 -0300

    Script parado, andando

--- knight-tale/Scripts/Player.gd
+++ knight-tale/Scripts/Player.gd
@@ -1,9 +1,15 @@
 extends CharacterBody2D
+
 @onready var container: Node2D = $Container
 @onready var animação: AnimatedSprite2D = $Container/AnimatedSprite2D
 @onready var espada: Area2D = $Container/Espada
 @onready var ataque: CollisionShape2D = $Container/Espada/Ataque
 
+var espadaItem = true
+var andando = false
+var pulando = false
+var parado = false
+var caindo = false
 
 const velocidade = 300.0
 const velocidade_pulo = -400.0
@@ -11,25 +17,60 @@ const gravidade = 900
 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	# gravidade
 	if not is_on_floor():
 		velocity.y += gravidade * delta
 
-	# Handle jump.
+	#Pulo.
 	if Input.is_action_just_pressed("pulo") and is_on_floor():
 		velocity.y = velocidade_pulo
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	
+	# Pega imput
 	var direção := Input.get_axis("esquerda", "direita")
-	if direção !=0:
-		velocity.x = direção * velocidade
-		animação.play("Caminhando com espada")
-		
-		container.scale.x = direção
-	else:
-		velocity.x = move_toward(velocity.x, 0, velocidade)
-		animação.play("Parado com espada")
-		
+	
+	if is_on_floor():
+		if direção !=0: #caminha
+			velocity.x = direção * velocidade
+			container.scale.x = direção
+			parado = false
+			andando = true
+			atualizar_animacão()
+		else: #parado
+			velocity.x = move_toward(velocity.x, 0, velocidade)
+			parado = true
+			andando = false
+			atualizar_animacão()
+	elif not is_on_floor:
+		if velocity.y > 0:
+			pulando = true
+			atualizar_animacão()
+		elif velocity.y <0:
+			caindo = true
+			atualizar_animacão()
 	
 	move_and_slide()
+
+func atualizar_animacão():
+	if andando:
+		if espadaItem == true:
+			animação.play("Andando com espada")
+		elif espadaItem == false:
+			animação.play("Andando sem espada")
+		return
+	
+	if parado:
+		if espadaItem == true:
+			animação.play("Parado com espada")
+		elif espadaItem == false:
+			animação.play("Parado sem espada")
+		return
+	
+	if pulando:
+		if espadaItem == true:
+			animação.play("Pulo com espada")
+		return
+	
+	if caindo:
+		if espadaItem == true:
+			animação.play("Caindo com espada")
+		return
