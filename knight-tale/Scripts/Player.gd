8b120a709ba00311dc5026868f78a55e884fd1f6
Marilu <Maria1luiza1s.s@gmail.com>
2026-03-18 20:00:57 -0300

    tentando fazer ataque

--- knight-tale/Scripts/Player.gd
+++ knight-tale/Scripts/Player.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 #Chamando nós que vamos usar no script
 @onready var animação: AnimatedSprite2D = $Container/AnimatedSprite2D
 @onready var espada: Area2D = $Container/Espada
-@onready var ataque: CollisionShape2D = $Container/Espada/Ataque
+@onready var ataqueArea: CollisionShape2D = $Container/Espada/Ataque
 @onready var container: Node2D = $Container
 
 # Variáveis pra sinais
@@ -15,6 +15,9 @@ const velocidade = 300.0
 const velocidade_pulo = -400.0
 const gravidade = 900
 
+# Variáveis de imput
+var direção
+var ataque
 # Função que roda o tempo todo da cena, focado na parte física
 func _physics_process(delta: float) -> void:
 	# gravidade
@@ -26,7 +29,8 @@ func _physics_process(delta: float) -> void:
 		velocity.y = velocidade_pulo
 	
 	# Pega imput
-	var direção := Input.get_axis("esquerda", "direita")
+	direção = Input.get_axis("esquerda", "direita")
+	ataque = Input.is_action_just_pressed("Ataque leve")
 	move_and_slide()
 	
 	if is_on_floor():
@@ -36,6 +40,8 @@ func _physics_process(delta: float) -> void:
 			atualizar_animacão()
 		else: #parado
 			velocity.x = move_toward(velocity.x, 0, velocidade)
+			atualizar_animacão()
+		if ataque:
 			atualizar_animacão()
 	else:
 		if direção !=0: #caminha
@@ -63,8 +69,12 @@ func atualizar_animacão():
 				animação.play("Parado sem espada")
 			
 		
-		if Input.is_action_just_pressed("Ataque leve"):
-			animação.play("Ataque para baixo")
+		if ataque == Input.is_action_just_pressed("Ataque leve"):
+			sinal = null
+			if randf() > 0.5:
+				animação.play("Ataque para baixo")
+			else:
+				animação.play("Ataque para cima")
 	else:
 		if velocity.y < 0:
 			if espadaItem == true:
