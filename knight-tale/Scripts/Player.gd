da7ca42b2078164e0fc4c828fb1ad1e9ab281ba0
Marilu <Maria1luiza1s.s@gmail.com>
2026-03-22 15:29:36 -0300

    Feito sistema de ataque e variável genero

--- knight-tale/Scripts/Player.gd
+++ knight-tale/Scripts/Player.gd
@@ -6,9 +6,14 @@ extends CharacterBody2D
 @onready var ataqueArea: CollisionShape2D = $Container/Espada/Ataque
 @onready var container: Node2D = $Container
 
+# Variável de genero
+# False = Cavaleiro, True = Cavaleira
+var genero = false
+
 # Variáveis pra sinais
 var espadaItem = true
 var sinal = null
+var sinalAtaque = null
 
 # Variáveis pra física
 const velocidade = 300.0
@@ -18,6 +23,7 @@ const gravidade = 900
 # Variáveis de imput
 var direção
 var ataque
+
 # Função que roda o tempo todo da cena, focado na parte física
 func _physics_process(delta: float) -> void:
 	# gravidade
@@ -30,7 +36,12 @@ func _physics_process(delta: float) -> void:
 	
 	# Pega imput
 	direção = Input.get_axis("esquerda", "direita")
-	ataque = Input.is_action_just_pressed("Ataque leve")
+	
+	ataque = Input.is_action_pressed("Ataque leve")
+	if ataque:
+		if sinalAtaque: # Sinal para não interromper animação
+			return
+		atualizar_animacão()
 	move_and_slide()
 	
 	if is_on_floor():
@@ -41,8 +52,6 @@ func _physics_process(delta: float) -> void:
 		else: #parado
 			velocity.x = move_toward(velocity.x, 0, velocidade)
 			atualizar_animacão()
-		if ataque:
-			atualizar_animacão()
 	else:
 		if direção !=0: #caminha
 			velocity.x = direção * velocidade
@@ -51,42 +60,58 @@ func _physics_process(delta: float) -> void:
 
 # Gerencia as animações pra evitar que entrem em conflito
 func atualizar_animacão():
-	if is_on_floor():
+	if ataque:
+		var probabilidade = randf()
+		sinalAtaque = true
+		
+		if probabilidade < 0.5:
+			if genero == false:
+				animação.play("Ataque para baixo")
+				print("ataque baixo")
+		else:
+			if genero == false:
+				animação.play("Ataque para cima")
+				print("ataque cima")
+		
+		await animação.animation_finished
+		sinalAtaque = false
+	
+	if is_on_floor() and not sinalAtaque: # Andando
 		if velocity.x != 0:
 			sinal = null
 			if espadaItem == true:
-				animação.play("Andando com espada")
+				if genero == false:
+					animação.play("Andando com espada")
 			elif espadaItem == false:
-				animação.play("Andando sem espada")
-			
+				if genero == false:
+					animação.play("Andando sem espada")
 		
-		else:
-			if sinal == animação.animation_finished:
-				await sinal
+		else: # Parado
+			if sinal == true:
+				await animação.animation_finished
+			
 			if espadaItem == true:
-				animação.play("Parado com espada")
+				if genero == false:
+					animação.play("Parado com espada")
 			elif espadaItem == false:
-				animação.play("Parado sem espada")
-			
-		
-		if ataque == Input.is_action_just_pressed("Ataque leve"):
-			sinal = null
-			if randf() > 0.5:
-				animação.play("Ataque para baixo")
-			else:
-				animação.play("Ataque para cima")
-	else:
+				if genero == false:
+					animação.play("Parado sem espada")
+	
+	else: # Pulo e queda
 		if velocity.y < 0:
 			if espadaItem == true:
-				animação.play("Pulo com espada")
-			else:
-				animação.play("Pulo sem espada")
+				if genero == false:
+					animação.play("Pulo com espada")
+			if espadaItem == false:
+				if genero == false:
+					animação.play("Pulo sem espada")
 			return
 		
 		if velocity.y > 0 :
 			if espadaItem == true:
-				animação.play("Caindo com espada")
+				if genero == false:
+					animação.play("Caindo com espada")
 			else:
-				animação.play("Caindo sem espada")
-			sinal = animação.animation_finished
-			return
+				if genero == false:
+					animação.play("Caindo sem espada")
+			sinal = true
