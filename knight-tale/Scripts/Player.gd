e87ea9f45d329c4f7ecfb6ea0abcebd7de893337
Marilu <Maria1luiza1s.s@gmail.com>
2026-03-26 15:48:30 -0300

    Update Player.gd
    testando discord

--- knight-tale/Scripts/Player.gd
+++ knight-tale/Scripts/Player.gd
@@ -24,7 +24,7 @@ const gravidade = 900
 var direção
 var ataque
 
-# Testando discord notificação denovo 2
+# Testando discord notificação denovo 3
 
 # Função que roda o tempo todo da cena, focado na parte física
 func _physics_process(delta: float) -> void:
